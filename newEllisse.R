f5037749e4aa8f0c0b156b971d6ba6944e4b942c
OttaviaE <otta.epifania@gmail.com>
2022-09-19 17:28:24 +0200

    Relation rule basic

--- newEllisse.R
+++ newEllisse.R
@@ -65,6 +65,36 @@ stimElli = function(object, rotation = NULL,
                     plot = T)
       }
       }
+  } # rotation and line ------
+  else if (!is.null(rotation) & is.null(color) & !is.null(line)) {
+    par(mfrow=c(3,3), mar = c(0.5,6,0.5,2)+0.1)
+    for (i in 1:length(rot_rule)) {
+      for (j in 1:length(rot_rule[[i]])) {
+        temp = NULL
+        Canvas(15, 15)
+        DrawEllipse(x = 0,
+                    radius.x = 10, 
+                    radius.y = 15, 
+                    lwd = 2, 
+                    rot = rot_rule[[i]][[j]], 
+                    plot = T, lty = line_rule[[i]][[j]])
+      }
+    } 
+  } # color and line ------
+  else if (is.null(rotation) & !is.null(color) & !is.null(line)) {
+    par(mfrow=c(3,3), mar = c(0.5,6,0.5,2)+0.1)
+    for (i in 1:length(rot_rule)) {
+      for (j in 1:length(rot_rule[[i]])) {
+        temp = NULL
+        Canvas(15, 15)
+        DrawEllipse(x = 0,
+                    radius.x = 10, 
+                    radius.y = 15, 
+                    lwd = 2, 
+                    plot = T, lty = line_rule[[i]][[j]], 
+                    col = SetAlpha("black",shade_rule[[i]][[j]]))
+      }
+    }
   } # color rotation line ----- 
   else if (!is.null(rotation) & !is.null(color) & !is.null(line)) {
     par(mfrow=c(3,3), mar = c(0.5,6,0.5,2)+0.1)
--- triangolo.R
+++ triangolo.R
@@ -6,7 +6,7 @@ stimTri = function(object, rotation = NULL,
   rot_rule = object[["rotation"]]
   shade_rule = object[["shade"]]
   line_rule = object[["line"]]
-  # rotaion only ----
+  # rotation only ----
   if (!is.null(rotation) & is.null(color) & is.null(line)) {
     par(mfrow=c(3,3), mar = c(0.5,6,0.5,2)+0.1)
     for (i in 1:length(rot_rule)) {
@@ -16,16 +16,58 @@ stimTri = function(object, rotation = NULL,
         DrawRegPolygon(rot = rot_rule[[i]][[j]], nv=3, lwd = 2)
       }
     }
-  } # rotation and color ---- 
+  }  # color only  ---- 
+  else if (is.null(rotation) & !is.null(color) & is.null(line)) {
+    par(mfrow=c(3,3), mar = c(0.5,6,0.5,2)+0.1)
+    for (i in 1:length(rot_rule)) {
+      for (j in 1:length(rot_rule[[i]])) {
+        temp = NULL
+        Canvas()
+        DrawRegPolygon(col = SetAlpha("black",shade_rule[[i]][[j]]),
+                       nv=3, lwd = 2)
+      }
+    }
+  } # line only ------
+  else if(is.null(rotation) & is.null(color) & !is.null(line)){
+    par(mfrow=c(3,3), mar = c(0.5,6,0.5,2)+0.1)
+    for (i in 1:length(rot_rule)) {
+      for (j in 1:length(rot_rule[[i]])) {
+        temp = NULL
+        Canvas()
+        DrawRegPolygon(lty = line_rule[[i]][[j]],
+                       nv=3, lwd = 2)
+      }
+    }
+  }
+  # rotation and color ---- 
   else if (!is.null(rotation) & !is.null(color) & is.null(line)) {
     par(mfrow=c(3,3), mar = c(0.5,6,0.5,2)+0.1)
     for (i in 1:length(rot_rule)) {
       for (j in 1:length(rot_rule[[i]])) {
         temp = NULL
         Canvas()
-        DrawRegPolygon(rot = rot_rule[[i]][[j]],
-                       col = SetAlpha("black",shade_rule[[i]][[j]]),
+        DrawRegPolygon(lty = line_rule[[i]][[j]],
                        nv=3, lwd = 2)
+      }
+    }
+  } else if (is.null(rotation) & !is.null(color) & !is.null(line)) { # line and color ----- 
+    par(mfrow=c(3,3), mar = c(0.5,6,0.5,2)+0.1)
+    for (i in 1:length(rot_rule)) {
+      for (j in 1:length(rot_rule[[i]])) {
+        temp = NULL
+        Canvas()
+        DrawRegPolygon(col = SetAlpha("black",shade_rule[[i]][[j]]),
+                       nv=3, lwd = 2, lty = line_rule[[i]][[j]])
+      }
+    }
+  } else if (!is.null(rotation) & is.null(color) & !is.null(line)) { # rotation and line ----
+    par(mfrow=c(3,3), mar = c(0.5,6,0.5,2)+0.1)
+    for (i in 1:length(rot_rule)) {
+      for (j in 1:length(rot_rule[[i]])) {
+        temp = NULL
+        Canvas()
+        DrawRegPolygon(rot = rot_rule[[i]][[j]],,
+                       nv=3, lwd = 2, lty = line_rule[[i]][[j]])
       }
     }
   } # rotation color and line ---- 
