e9394d9b604e48dc2919e57943e8fc0564bd4618
OttaviaE <otta.epifania@gmail.com>
2022-09-27 10:38:49 +0200

    new

--- twoFunctions.R
+++ twoFunctions.R
@@ -30,55 +30,68 @@ getDone = function(object,
                    shade = c("wtg", "wtb", "btw", "btg"), 
                    line = c("sdad", "dads", "sdda", "ddas")){
   the_rule = list()
-  # which_rots
-  h = pi/2; d= pi/3; v = 0
-  rot_rule = list()
-  # colors 
-  sw = 0; sg = 0.10; sb = 0.6
-  shade_rule = list()
-  # which_line 
-  s = 1; da = 5; do = 3
-  line_rule = list()
+  # empty rotation 
+  empty_rot = matrix(start[["rot"]], nrow = 3, ncol = 3)
+  # empty shade 
+  empty_shade = matrix(start[["shade"]], nrow = 3, ncol = 3)
+  # empty line 
+  empty_line = matrix(start[["rot"]], nrow = 3, ncol = 3)
+  # base rotation 
+  base_rot = c(rep(0, 3))
+  # base shade 
+  base_shade = c(rep(0, 3))
+  # base line 
+  base_line = c(rep(1, 3))
+  
+  # htv 
+  htv = c(h = pi/2, d= pi/3, v = 0)
+  # vth 
+  vth = c(v=0, d=pi/3, h=pi/2)
+ 
+  # wtb
+  wtb = c(sw = 0, sg = 0.10, sb = 0.6)
+  # wtg 
+  wtg = c(sw = 0,  sb = 0.6, sg = 0.10)
+  
+  # sdad 
+  sdad = c(s = 1, da = 5, do = 3)
+  # dads
+  dads = c(da = 5, do = 3, s = 1)
   
   if (object == "rotation") {
     if (rotation == "htv") {
-      htv = c(h = pi/2, d= pi/3, v = 0)
       start = list(rot = htv, 
-                   shade = c(rep(0,3)), 
-                   line = c(rep(1, 3)))
+                   shade = base_shade, 
+                   line = base_line)
       
     } else if (rotation == "vth") {
-      vth = c(v=0, d=pi/3, h=pi/2)
       start = list(rot = htv, 
-                   shade = c(rep(0,3)), 
-                   line = c(rep(1, 3)))
+                   shade = base_shade, 
+                   line = base_line)
     }
     rest = list(rotation = Permn(start[["rot"]])[c(seq(1,5,by=2)), ], 
-                shade =matrix(start[["shade"]], nrow = 3, ncol = 3), 
-                line = matrix(start[["line"]], nrow = 3, ncol = 3))
+                shade = empty_shade, 
+                line = empty_line)
   } else if (object == "shade") {
     if (shade == "wtb") {
-      wtg = c(sw = 0, sg = 0.10, sb = 0.6)
-      start = list(rot = c(rep(0,3)), 
-                   shade = wtg, 
-                   line = c(rep(1, 3)))
+      start = list(rot = base_rot, 
+                   shade = wtb, 
+                   line = base_line)
     } else if (shade == "wtg") {
-      wtg = c(sw = 0,  sb = 0.6, sg = 0.10)
-      start = list(rot = c(rep(0,3)), 
+      start = list(rot = base_rot, 
                    shade = wtg, 
-                   line = c(rep(1, 3)))
+                   line = base_line)
     }
-    rest = list(rotation = matrix(start[["rot"]], nrow = 3, ncol = 3), 
+    rest = list(rotation = empty_rot, 
                 shade =Permn(start[["shade"]])[c(seq(1,5,by=2)), ], 
-                line = matrix(start[["line"]], nrow = 3, ncol = 3))
+                line = empty_line)
   } else if (object == "line") {
     if (line == "sdad") {
-      sdad = c(s = 1, da = 5, do = 3)
+      
       start = list(rot = c(rep(0,3)), 
                    shade = c(rep(0,3)), 
                    line = sdad)
     } else if (line == "dads") {
-      dads = c(da = 5, do = 3, s = 1)
       start = list(rot = c(rep(0,3)), 
                    shade = c(rep(0,3)), 
                    line = dads)
@@ -86,7 +99,20 @@ getDone = function(object,
     rest = list(rotation = matrix(start[["rot"]], nrow = 3, ncol = 3), 
                 shade =matrix(start[["shade"]], nrow = 3, ncol = 3), 
                 line =Permn(start[["line"]])[c(seq(1,5,by=2)), ])
-  }   
+  } else if (object == "rot_shade") {
+    if (rotation == "htv" & shade == "wtg") {
+      start = list(rot = htv, 
+                   shade = wtg, 
+                   line = base_line)
+    } else if (rotation == "htv" & shade == "wtb") {
+      start = list(rot = htv, 
+                   shade = wtb, 
+                   line = base_line)
+    }
+    rest = list(rotation = Permn(start[["rot"]])[c(seq(1,5,by=2)), ], 
+                shade = Permn(start[["rot"]])[c(seq(1,5,by=2)), ], 
+                line= empty_line)
+  }  
   the_rule = rest
   return(the_rule)
   }
