06f530176373bac36f85d03dd77daa6e374b505e
OttaviaE <otta.epifania@gmail.com>
2022-09-28 12:56:52 +0200

    new

--- twoFunctions.R
+++ twoFunctions.R
@@ -1,8 +1,9 @@
 # 27/09 ----
 # provo a creare le regole gerarchiche con integrate le permutazioni
-
-highRule = function(rotation = NULL, shade = NULL, line = NULL) {
-  if (is.null(rotation) & is.null(shade) & is.null(line)) {
+rm(list = ls()[!ls() %in% "stimElli"])
+highRule = function(rotation = NULL, shade = NULL, line = NULL, 
+                    multi = F) {
+  if (is.null(rotation) & is.null(shade) & is.null(line) & multi == F) {
     stop("Please specificy an argument") 
   } else if (!is.null(rotation) & is.null(shade) & is.null(line)){
     ruleG = "rotation"
@@ -18,6 +19,8 @@ highRule = function(rotation = NULL, shade = NULL, line = NULL) {
     ruleG = "shade"
   } else if(is.null(rotation) & is.null(shade) & !is.null(line)) {
     ruleG = "line"
+  } else if (multi == T) {
+    ruleG = "multi"
   }
   return(ruleG)
 }
@@ -28,20 +31,16 @@ highRule(shade = T)
 getDone = function(object, 
                    rotation = c("htv", "vth", "dtv", "dth"), 
                    shade = c("wtg", "wtb", "btw", "btg"), 
-                   line = c("sdad", "dads", "sdda", "ddas")){
+                   line = c("sdad", "dads", "sdda", "ddas"), 
+                   multi = c("increasing", "decreasing")){
   the_rule = list()
   # empty rotation 
-  empty_rot = matrix(start[["rot"]], nrow = 3, ncol = 3)
+  empty_rot = matrix(0, nrow = 3, ncol = 3)
   # empty shade 
-  empty_shade = matrix(start[["shade"]], nrow = 3, ncol = 3)
+  empty_shade = matrix(0, nrow = 3, ncol = 3)
   # empty line 
-  empty_line = matrix(start[["rot"]], nrow = 3, ncol = 3)
-  # base rotation 
-  base_rot = c(rep(0, 3))
-  # base shade 
-  base_shade = c(rep(0, 3))
-  # base line 
-  base_line = c(rep(1, 3))
+  empty_line = matrix(1, nrow = 3, ncol = 3)
+  
   
   # htv 
   htv = c(h = pi/2, d= pi/3, v = 0)
@@ -49,9 +48,9 @@ getDone = function(object,
   vth = c(v=0, d=pi/3, h=pi/2)
  
   # wtb
-  wtb = c(sw = 0, sg = 0.10, sb = 0.6)
+  wtb = c(sw = 0, sg = 0.20, sb = 0.8)
   # wtg 
-  wtg = c(sw = 0,  sb = 0.6, sg = 0.10)
+  wtg = c(sw = 0,  sb = 0.8, sg = 0.20)
   
   # sdad 
   sdad = c(s = 1, da = 5, do = 3)
@@ -60,27 +59,19 @@ getDone = function(object,
   
   if (object == "rotation") {
     if (rotation == "htv") {
-      start = list(rot = htv, 
-                   shade = base_shade, 
-                   line = base_line)
+      start = list(rot = htv)
       
     } else if (rotation == "vth") {
-      start = list(rot = htv, 
-                   shade = base_shade, 
-                   line = base_line)
+      start = list(rot = htv)
     }
     rest = list(rotation = Permn(start[["rot"]])[c(seq(1,5,by=2)), ], 
                 shade = empty_shade, 
                 line = empty_line)
   } else if (object == "shade") {
     if (shade == "wtb") {
-      start = list(rot = base_rot, 
-                   shade = wtb, 
-                   line = base_line)
+      start = list(shade = wtb)
     } else if (shade == "wtg") {
-      start = list(rot = base_rot, 
-                   shade = wtg, 
-                   line = base_line)
+      start = list(shade = wtg)
     }
     rest = list(rotation = empty_rot, 
                 shade =Permn(start[["shade"]])[c(seq(1,5,by=2)), ], 
@@ -88,35 +79,88 @@ getDone = function(object,
   } else if (object == "line") {
     if (line == "sdad") {
       
-      start = list(rot = c(rep(0,3)), 
-                   shade = c(rep(0,3)), 
-                   line = sdad)
+      start = list(line = sdad)
     } else if (line == "dads") {
-      start = list(rot = c(rep(0,3)), 
-                   shade = c(rep(0,3)), 
-                   line = dads)
+      start = list(line = dads)
     }
-    rest = list(rotation = matrix(start[["rot"]], nrow = 3, ncol = 3), 
-                shade =matrix(start[["shade"]], nrow = 3, ncol = 3), 
+    rest = list(rotation = empty_rot, 
+                shade = empty_shade, 
                 line =Permn(start[["line"]])[c(seq(1,5,by=2)), ])
-  } else if (object == "rot_shade") {
-    if (rotation == "htv" & shade == "wtg") {
+  } else if (object == "rot_shade") { # rot_shade -----
+    if (rotation == "htv" & shade == "wtg") { # htv and wtg ----
       start = list(rot = htv, 
-                   shade = wtg, 
-                   line = base_line)
-    } else if (rotation == "htv" & shade == "wtb") {
+                   shade = wtg)
+    } else if (rotation == "htv" & shade == "wtb") { # htv and wtg -----
       start = list(rot = htv, 
-                   shade = wtb, 
-                   line = base_line)
+                   shade = wtb)
     }
     rest = list(rotation = Permn(start[["rot"]])[c(seq(1,5,by=2)), ], 
                 shade = Permn(start[["rot"]])[c(seq(1,5,by=2)), ], 
                 line= empty_line)
-  }  
+  } else if (object == "rot_line") {
+    if (rotation == "htv" & line == "sdad") { # htv and wtg ----
+      start = list(rot = htv, 
+                   line = sdad)
+    } else if (rotation == "htv" & line == "dads") { # htv and wtg -----
+      start = list(rot = htv, 
+                   line = dads)
+    }
+    rest = list(rotation = Permn(start[["rot"]])[c(seq(1,5,by=2)), ], 
+                shade = empty_shade, 
+                line= Permn(start[["line"]])[c(seq(1,5,by=2)), ])
+  } else if (object == "shade_line") {
+    if (shade == "wtb" & line == "sdad") { # htv and wtg ----
+      start = list(shade = wtb, 
+                   line = sdad)
+    } else if (shade == "wtg" & line == "sdad") { # htv and wtg -----
+      start = list( shade = wtg, 
+                   line = sdad)
+    }
+    rest = list(rotation = empty_rot, 
+                shade = Permn(start[["shade"]])[c(seq(1,5,by=2)), ], 
+                line= Permn(start[["line"]])[c(seq(1,5,by=2)), ])
+  } else if (object == "all") {
+    if (rotation == "htv" & shade == "wtb" & line == "sdad") {
+      start = list(rot = htv, 
+                   shade = wtb, 
+                   line = sdad)
+    } else if (rotation == "vth" & shade == "wtb" & line == "sdad") {
+      start = list(rot = vth, 
+                   shade = wtb, 
+                   line = sdad)
+    }
+    rest = list(rotation = Permn(start[["rot"]])[c(seq(1,5,by=2)), ], 
+                shade = Permn(start[["shade"]])[c(seq(1,5,by=2)), ], 
+                line= Permn(start[["line"]])[c(seq(1,5,by=2)), ])
+  } else if (multi == "increasing") {
+    rest = matrix(c(c(1:3), c(2,1,3), c(3:1)), nrow=3, ncol= 3, byrow = T)
+  } else if (multi == "decreasing") {
+    rest = matrix(c(c(3:1), c(2,1,3), c(1:3),), nrow=3, ncol= 3, byrow = T)
+  }
   the_rule = rest
   return(the_rule)
   }
+matrix(c(c(1:3), c(3:1), c(2,1,3)), nrow=3, ncol= 3, byrow = T)
 
-stimElli(getDone(highRule(rotation = T), rotation = "htv"))
-stimElli(getDone(highRule(shade = T), shade = "wtb"))
+stimElli(getDone(highRule(multi = T), multi = "increasing"))
+
+stimElli(getDone(highRule(multi = T), multi = "increasing"))
+getDone(highRule(rotation = T), rotation = "htv")
+cont = prova[which(is.null(prova)) == F]
+
+prova = (getDone(highRule(shade = T), shade = "wtb"))
+
 stimElli(getDone(highRule(line = T), line = "sdad"))
+stimElli(getDone(highRule(line = T, rotation = T), line = "sdad", 
+                 rotation = "htv"))
+stimElli(getDone(highRule(line = T, shade = T), line = "sdad", 
+                 shade = "wtg"))
+
+prova = (getDone(highRule(line = T, shade = T, rotation = T), 
+                 line = "sdad", 
+                 shade = "wtb", 
+                 rotation = "htv"))
+p = getDone(highRule(multi = T), multi = "increasing")
+
+stimElli(getDone(highRule(multi = T), multi = "increasing"))
+highRule(rotation  = T)
