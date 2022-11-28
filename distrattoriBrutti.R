a7df47492c6a6b543be8c5d08cfbdf2a70c9c904
OttaviaE <otta.epifania@gmail.com>
2022-11-28 18:05:57 +0100

    cose nuove

--- distrattoriBrutti.R
+++ distrattoriBrutti.R
@@ -20,7 +20,8 @@ repetition = function(m,which = "all") {
           na.rm = T) == F) {
     warning("R-Top is equal to the correct response")
     flag = "r.top"
-  } else if (any(unlist(distr.repetition$r.left) != unlist(m.correct), na.rm = T) == F) {
+  } else if (any(unlist(distr.repetition$r.left) != unlist(m.correct), 
+                 na.rm = T) == F) {
     warning("R-left is equal to the correct response")
     flag = "r.left"
   } else if (any(unlist(distr.repetition$r.diag) != unlist(m.correct), 
@@ -34,6 +35,8 @@ repetition = function(m,which = "all") {
 
 
 wp = function(m, which = "all") {
+  m.correct = correct(m)
+  check.rep = repetition(m)
   sample.index = sample(c(1:4, 7)) # non è random
   s = sample.index[1]
   distr.wp.copy = m[[s]]
@@ -41,6 +44,10 @@ wp = function(m, which = "all") {
   for (i in 2:8) {
     distr.wp.matrix = cof(distr.wp.matrix, 
                           m[[i]])
+  }
+  if (any(unlist(distr.wp.copy) != unlist(m.correct), 
+          na.rm = T) == F) {
+    warning("WP-Copy is equal to the correct response!")
   }
   distr.wp = list(wp.copy = distr.wp.copy, 
                   wp.matrix = distr.wp.matrix)
@@ -89,4 +96,34 @@ d.union = function(m, n = 1,
   
   d.union = cof(d.union, obj)
   return(d.union)
+}
+
+
+
+ic = function(m, 
+              n.rule = 1) {
+  m.correct = correct(m)
+  
+  ic.scale = size(m.correct, 3)
+  ic.flip = rotation(correct(m), 3) # il numero sulla rotazione dipende dalla figura
+  ic.inc = "nope"
+  if (n.rule != 1) {
+    ic.inc = hide(m.correct, 2)
+  }
+  ic.dist = list(ic.scale = ic.scale, 
+                 ic.flip = ic.flip, 
+                 ic.inc = ic.inc)
+  return(ic.dist)
+}
+
+draw.distr = function(obj.list) {
+  if (length(obj.list) == 5) {
+    par(mfrow = c(1, 5))
+  } else {
+    par(mfrow=c(2, 4))
+  }
+  
+  for (i in 1:length(obj.list)) {
+    draw(obj.list[[i]])
+  }
 }
